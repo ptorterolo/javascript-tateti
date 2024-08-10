008c78cb489dd411c1cc97e52ad33e601df8a3af
Pablo Torterolo <ptorterolo@gmail.com>
2024-08-10 16:11:31 -0300

    initial board render, prettier file added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import "./styles.css";
+import './styles.css'
 class TicTacToeGame {
-  
-  constructor() {}
+    constructor() {
+        this.board = Array(9).fill(null)
+        this.restartButton = document.getElementById('restart-button')
+        this.init()
+    }
+    init() {
+        this.renderBoard()
+    }
+    renderBoard() {
+        const boardElement = document.getElementById('game-board')
+        boardElement.classList.add(
+            'grid',
+            'grid-cols-3',
+            'gap-4',
+            'bg-purple-900',
+            'rounded-lg',
+            'py-8',
+            'px-4'
+        )
+        boardElement.innerHTML = ''
+        this.board.forEach((cell, index) => {
+            const cellDiv = document.createElement('div')
+            cellDiv.classList.add('cell')
+            cellDiv.setAttribute('data-index', index)
+            boardElement.appendChild(cellDiv)
+        })
+    }
 }
 
-let game = new TicTacToeGame();
+let game = new TicTacToeGame()
