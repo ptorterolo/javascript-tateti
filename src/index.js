6a124e6bb52828d96226bc2dbc61af2453f81a08
Pablo Torterolo <ptorterolo@gmail.com>
2024-08-10 23:41:40 -0300

    user can click on cells, values will be saved, player will change

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ class TicTacToeGame {
     constructor() {
         this.board = Array(9).fill(null)
         this.restartButton = document.getElementById('restart-button')
+        this.statusDisplay = document.getElementById('game-status')
+        this.currentPlayer = 'X'
+        this.gameState = ['', '', '', '', '', '', '', '', '']
+        this.gameActive = true
         this.init()
     }
     init() {
         this.renderBoard()
+        this.updateStatusDisplay()
     }
     renderBoard() {
         const boardElement = document.getElementById('game-board')
@@ -20,12 +25,45 @@ class TicTacToeGame {
             'px-4'
         )
         boardElement.innerHTML = ''
-        this.board.forEach((cell, index) => {
+        this.board.forEach((_, index) => {
             const cellDiv = document.createElement('div')
             cellDiv.classList.add('cell')
             cellDiv.setAttribute('data-index', index)
+            cellDiv.addEventListener('click', this.handleCellClick.bind(this))
             boardElement.appendChild(cellDiv)
         })
+    }
+
+    handleCellClick(event) {
+        const clickedCell = event.target
+
+        const clickedCellIndex = parseInt(
+            clickedCell.getAttribute('data-index')
+        )
+
+        if (this.gameState[clickedCellIndex] !== '' || !this.gameActive) {
+            return
+        }
+
+        this.handleCellPlayed(clickedCell, clickedCellIndex)
+        this.handlePlayerChange()
+    }
+
+    handlePlayerChange() {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'
+        this.updateStatusDisplay()
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex) {
+        this.gameState[clickedCellIndex] = this.currentPlayer
+        clickedCell.innerHTML = this.currentPlayer
+        const cellColor =
+            this.currentPlayer === 'X' ? 'text-cyan-500' : 'text-amber-500'
+        clickedCell.classList.add(cellColor)
+    }
+
+    updateStatusDisplay() {
+        this.statusDisplay.innerHTML = `Es el turno de ${this.currentPlayer}`
     }
 }
 
